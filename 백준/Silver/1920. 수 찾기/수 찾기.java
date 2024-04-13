3b045f905d6dbabcd8785b8b6ed9ddd272d3caf6
shlim0287 <112301522+shlim0287@users.noreply.github.com>
2024-04-13 22:24:39 +0900

    [Silver IV] Title: 수 찾기, Time: 808 ms, Memory: 52488 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+      public static int [] arrN;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        arrN=new int [n];
+
+        for(int i=0;i<n;i++){
+            arrN[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arrN);
+
+        int m=Integer.parseInt(br.readLine());
+        st=new StringTokenizer(br.readLine());
+
+
+        for(int i=0;i<m;i++){
+            int num = Integer.parseInt(st.nextToken());
+            bw.write(binarySearch(num) + " ");
+            
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static int binarySearch(int num){
+        int left=0;
+        int right = arrN.length - 1;
+        
+        while(left<=right){
+            int middle=(left+right)/2;
+            int middleValue=arrN[middle];
+            
+            if(num>middleValue){
+                left=middle+1;
+            }else if(num<middleValue){
+                right=middle-1;
+            }else return 1;
+            
+        }
+        return 0;
+    }
+
+}
