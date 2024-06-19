778b6560552e67710ea5d7f066da06fef433beba
shlim0287 <112301522+shlim0287@users.noreply.github.com>
2024-06-19 15:03:51 +0900

    [Silver II] Title: DNA 비밀번호, Time: 288 ms, Memory: 25020 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,101 +1,79 @@
 import java.util.*;
 import java.io.*;
 public class Main{
-    static int [] myArr;
-    static int [] checkArr;
+    static int[] checkArr;
+    static int[] myArr;
     static int checkSecret;
-    
     public static void main(String[] args)throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
         int s=Integer.parseInt(st.nextToken());
         int p=Integer.parseInt(st.nextToken());
-        int result=0;
-        myArr = new int[4];
-        checkArr= new int[4];
-        char A[] = new char[s];
+        checkArr=new int[4];
+        myArr=new int [4];
         checkSecret=0;
-        
+        int result=0;
+        char[] A=new char[s];
         A=br.readLine().toCharArray();
+        
         st=new StringTokenizer(br.readLine());
+        
         for(int i=0;i<checkArr.length;i++){
             checkArr[i]=Integer.parseInt(st.nextToken());
-            if(checkArr[i]==0){
-                checkSecret++;
-            }
+            if(checkArr[i]==0)checkSecret++;
         }
+        
         for(int i=0;i<p;i++){
             add(A[i]);
         }
-        if(checkSecret==4){
-            result++;
-        }
+            if(checkSecret==4)result++;
         
-        //슬라이딩 윈도우
         for(int i=p;i<s;i++){
             int j=i-p;
             add(A[i]);
             remove(A[j]);
-            if(checkSecret==4){
-            result++;
-            }
+            if(checkSecret==4)result++;
         }
         System.out.println(result);
+        
         br.close();
     }
-    private static void add(char i){
-        switch(i){
+    private static void add(char c){
+        switch(c){
                 case'A':
                 myArr[0]++;
-                if(myArr[0]==checkArr[0]){
-                    checkSecret++;
-                }
+                    if(myArr[0]==checkArr[0])checkSecret++;
                 break;
                 case'C':
                                 myArr[1]++;
-                if(myArr[1]==checkArr[1]){
-                    checkSecret++;
-                }
+                    if(myArr[1]==checkArr[1])checkSecret++;
                 break;
-                
                 case'G':
                                 myArr[2]++;
-                if(myArr[2]==checkArr[2]){
-                    checkSecret++;
-                }
+                    if(myArr[2]==checkArr[2])checkSecret++;
                 break;
                 case'T':
                                 myArr[3]++;
-                if(myArr[3]==checkArr[3]){
-                    checkSecret++;
-                }
+                    if(myArr[3]==checkArr[3])checkSecret++;
                 break;
         }
     }
-    private static void remove(char i){
-        switch(i){
+    private static void remove(char c){
+        switch(c){
                 case'A':
-                if(myArr[0]==checkArr[0]){
-                    checkSecret--;
-                }
+                if(myArr[0]==checkArr[0])checkSecret--;
                 myArr[0]--;
                 break;
                 case'C':
-                if(myArr[1]==checkArr[1]){
-                    checkSecret--;
-                }
+                if(myArr[1]==checkArr[1])checkSecret--;
                 myArr[1]--;
                 break;
                 case'G':
-                if(myArr[2]==checkArr[2]){
-                    checkSecret--;
-                }
+                if(myArr[2]==checkArr[2])checkSecret--;
                 myArr[2]--;
                 break;
                 case'T':
-                if(myArr[3]==checkArr[3]){
-                    checkSecret--;
-                }
+                if(myArr[3]==checkArr[3])checkSecret--;
                 myArr[3]--;
                 break;
         }
