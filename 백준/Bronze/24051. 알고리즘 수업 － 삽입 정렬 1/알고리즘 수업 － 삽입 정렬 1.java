554beb3aac6dfa8c95f75f68d5c73df827ef43d2
shlim0287 <112301522+shlim0287@users.noreply.github.com>
2024-06-27 21:10:15 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 236 ms, Memory: 16444 KB -BaekjoonHub

--- 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.java
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.java
@@ -1,51 +1,47 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // 입력
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
-        int[] A = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            A[i] = scanner.nextInt();
+import java.util.*;
+import java.io.*;
+public class Main{
+    static int n=0;
+    static int k=0;
+    static int[] a;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+       
+        n=Integer.parseInt(st.nextToken());
+        k=Integer.parseInt(st.nextToken());
+        a=new int [n];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            a[i]=Integer.parseInt(st.nextToken());
         }
         
-        // 삽입 정렬 및 K번째 저장되는 수 찾기
-        int result = findKthSave(N, K, A);
+        int result=findK(n,k,a);
         
-        // 결과 출력
         System.out.println(result);
     }
-
-    public static int findKthSave(int N, int K, int[] A) {
-        int saveCount = 0;
-
-        for (int i = 1; i < N; i++) {
-            int loc = i - 1;
-            int newItem = A[i];
-
-            // A[0..i-1] 부분은 이미 정렬된 상태
-            while (loc >= 0 && newItem < A[loc]) {
-                A[loc + 1] = A[loc];
+    public static int findK(int n,int k, int [] a){
+        int saveCount=0;
+        for(int i=1;i<n;i++){
+            int loc=i-1;
+            int newItem=a[i];
+            
+            while(loc>=0 && newItem<a[loc]){
+                a[loc+1]=a[loc];
                 loc--;
                 saveCount++;
-                if (saveCount == K) {
-                    return A[loc + 1];
+                if(saveCount==k){
+                    return a[loc+1];
                 }
             }
-            if (loc + 1 != i) {
-                A[loc + 1] = newItem;
+            if(loc+1!=i){
+                a[loc+1]=newItem;
                 saveCount++;
-                if (saveCount == K) {
+                if(saveCount==k){
                     return newItem;
                 }
             }
         }
-
-        // 저장 횟수가 K보다 작으면 -1 리턴
         return -1;
     }
 }
