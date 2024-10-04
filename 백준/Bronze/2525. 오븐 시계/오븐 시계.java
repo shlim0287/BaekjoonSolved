7bf7469db01e99083fcfa29d5a6ec771aa66d99b
limsung <112301522+shlim0287@users.noreply.github.com>
2024-10-04 22:34:24 +0900

    [Bronze III] Title: 오븐 시계, Time: 188 ms, Memory: 18440 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 public class Main{
-    public static void main(String[]args){
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int hour=sc.nextInt();
-        int min=sc.nextInt();
-        int ing=sc.nextInt();
         
-        int mins=60*hour+min+ing;
+        int h = sc.nextInt();
+        int m = sc.nextInt();
         
-        int hours=(mins/60)%24;
-        int minss=(mins%60);
-        System.out.printf("%d %d",hours,minss);
+        int run = sc.nextInt();
+        
+        int total=60*h+m;
+        total+=run;
+        
+        int hour=(total/60)%24;
+        int min=total%60;
+        
+        System.out.println(hour + " "+ min);
     }
 }
