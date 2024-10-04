8f2ba88c040c74e332a90c0c5c223e2a643172e9
limsung <112301522+shlim0287@users.noreply.github.com>
2024-10-05 00:38:35 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -4,9 +4,10 @@ public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         Set<Integer> set=new HashSet<>();
-        for(int i=0;i<10;i++){
-            int a=Integer.parseInt(br.readLine());
-            set.add(a%42);
+        
+        for(int i=1;i<=10;i++){
+            int num=Integer.parseInt(br.readLine())%42;
+            set.add(num);
         }
         System.out.println(set.size());
     }
