83ebf59cf37aa2222a869fa11b147b8bda1866a9
limsung <112301522+shlim0287@users.noreply.github.com>
2024-10-05 21:00:23 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,10 +1,11 @@
 import java.util.*;
-import java.io.*;
+//한번 나온 값이 이후에 다시 나오면 그룹 단어 x-> 이전 단어여부 파악,
+// 배열로 관리
+//반복해야하므로 별도의 메서드로 관리
 public class Main{
-   static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    
-    public static void main(String[] args)throws IOException{
-        int n=Integer.parseInt(br.readLine());
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args){
+        int n=sc.nextInt();
         int count=0;
         for(int i=0;i<n;i++){
             if(check()==true){
@@ -13,27 +14,28 @@ public class Main{
         }
         System.out.println(count);
         
-       
     }
-    public static boolean check() throws IOException{
-        boolean[] check=new boolean[26];
-        int b=0;
-        String str=br.readLine();
+    
+    private static boolean check(){
+        boolean[] arr=new boolean[26];
+        int prev=0;
+        String str=sc.next();
         
         for(int i=0;i<str.length();i++){
-            int now=str.charAt(i);
-            
-            if(b!=now){
-                if(check[now-'a']==false){
-                    check[now-'a']=true;
-                    b=now;
+            int now = str.charAt(i);
+            if(prev != now){// 틀리면?
+                if(arr[now-'a']==false){
+                    arr[now-'a']=true;
+                    prev=now;
                 }else{
                     return false;
                 }
-            }else{
+            }else{ //깉으면 지속
                 continue;
             }
         }
         return true;
     }
+    
+    
 }
