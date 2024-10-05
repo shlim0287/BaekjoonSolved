c76f7b951c11a46ee8287b92cc0faea86f9b5278
limsung <112301522+shlim0287@users.noreply.github.com>
2024-10-05 13:35:13 +0900

    [Bronze II] Title: 상수, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,38 +1,12 @@
-import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb=new StringBuilder();
-        StringBuilder sb2=new StringBuilder();
+        String[] numbers = br.readLine().split(" ");  
+        String num1 = new StringBuilder(numbers[0]).reverse().toString();
+        String num2 = new StringBuilder(numbers[1]).reverse().toString();
         
-        String[] num1=st.nextToken().split("");
-        String[] num2=st.nextToken().split("");
-        
-        int start=0;
-        int end=num1.length-1;
-        while(start<end){
-            String temp1=num1[start];
-            String temp2=num2[start];
-            
-            num1[start]=num1[end];
-            num2[start]=num2[end];
-            num1[end] = temp1;
-            num2[end] = temp2;
-            
-            start++;
-            end--;
-        }
-        for(int i=0;i<num1.length;i++){
-            sb.append(num1[i]);
-            sb2.append(num2[i]);
-        }
-        
-        
-        
-        System.out.println(Math.max(Integer.parseInt(sb.toString()),Integer.parseInt(sb2.toString())));
-        
+        System.out.println(Math.max(Integer.parseInt(num1), Integer.parseInt(num2)));
     }
 }
