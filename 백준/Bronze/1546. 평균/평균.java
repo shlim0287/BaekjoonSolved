41dd75e99daf8b679b9119dfc0c5d6acead44ea9
limsung <112301522+shlim0287@users.noreply.github.com>
2024-10-05 11:29:43 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 16500 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,24 @@
 import java.util.*;
 import java.io.*;
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
         
-        double arr[]=new double[Integer.parseInt(br.readLine())];
+        int n=Integer.parseInt(br.readLine());
+        double[] arr=new double[n];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i = 0; i < arr.length; i++) {
+			arr[i]=Double.parseDouble(st.nextToken());
+		}
         
-        StringTokenizer st=new StringTokenizer(br.readLine()," ");
-        for(int i=0;i<arr.length;i++){
-            arr[i]=Double.parseDouble(st.nextToken());
-        }
+        //합 구하기,최댓값 찾기
         double sum=0;
-        Arrays.sort(arr);
         
-        for(int i=0;i<arr.length;i++){
-            sum+=(arr[i]/arr[arr.length-1])*100;
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            sum+=(arr[i]/arr[n-1])*100;
         }
-        System.out.println(sum/arr.length);
+        
+        System.out.println(sum/n);
     }
 }
