340b86457cced9a0a5815c9eb5c89a907a9c4f96
limsung <112301522+shlim0287@users.noreply.github.com>
2024-10-08 12:20:38 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 584 ms, Memory: 141880 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,28 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main{
-    static ArrayList<Integer>[] a;
     static boolean[] visited;
+    static ArrayList<Integer>[] a;
     public static void main(String[] args)throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int n=Integer.parseInt(st.nextToken());
-        int m=Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        visited=new boolean[n+1]; 
-        a=new ArrayList[n+1];
+        int n = Integer.parseInt(st.nextToken()); //노드
+        int m = Integer.parseInt(st.nextToken());// 에지
+        
+        visited=new boolean[n+1]; // 인덱스 1번부터 고려
+        
+        a= new ArrayList[n+1];
         for(int i=1;i<n+1;i++){
             a[i]=new ArrayList<>();
         }
+        
         for(int i=0;i<m;i++){
             st=new StringTokenizer(br.readLine());
-            int s=Integer.parseInt(st.nextToken());
-            int e=Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken()); 
+            int e = Integer.parseInt(st.nextToken());
             a[s].add(e);
             a[e].add(s);
         }
+        int count = 0;
         
-        int count=0;
         for(int i=1;i<n+1;i++){
             if(!visited[i]){
                 count++;
@@ -30,9 +34,12 @@ public class Main{
             }
         }
         System.out.println(count);
+       
     }
-    public static void dfs(int v){
-        if(visited[v])return;
+    private static void dfs(int v){
+        if(visited[v]){
+            return;
+        }
         visited[v]=true;
         for(int i:a[v]){
             if(!visited[i]){
@@ -40,4 +47,5 @@ public class Main{
             }
         }
     }
+    
 }
