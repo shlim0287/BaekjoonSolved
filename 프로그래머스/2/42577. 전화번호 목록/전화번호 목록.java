d37a31aba01454078b26a902449cb617fbcb45cb
limsung <112301522+shlim0287@users.noreply.github.com>
2024-10-18 00:17:45 +0900

    [level 2] Title: 전화번호 목록, Time: 292.60 ms, Memory: 244 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,20 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        Set<String> set=new HashSet<>();
         
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
-            }
+        for(String p : phone_book){
+            set.add(p);
         }
         
+        for(String phone : phone_book){
+            for(int i=1;i<phone.length();i++){
+                String prefix=phone.substring(0,i);
+                if(set.contains(prefix)){
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }
