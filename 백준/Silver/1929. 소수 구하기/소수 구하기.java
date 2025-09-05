2e8b4ec80b09e138927e04c0954570c8ae05a08b
limsung <112301522+shlim0287@users.noreply.github.com>
2025-09-05 10:37:21 +0900

    [Silver III] Title: 소수 구하기, Time: 780 ms, Memory: 32868 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,38 +1,38 @@
 import java.util.*;
+
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int m= sc.nextInt();
-        int n= sc.nextInt();
         
-        ArrayList<Integer> primes = new ArrayList<>();
+        int m = sc.nextInt();
+        int n= sc.nextInt();
+        List<Integer> list = primes(m,n);
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        
+    }
+    private static List<Integer> primes(int m, int n){
+        boolean[] arr = new boolean[n+1];
+        Arrays.fill(arr, true);
+        arr[0] = false;
+        if(n>=1){
+            arr[1]=false;
+        }
+        
+        for(int i=2;i*i<=n;i++){
+            if(arr[i]){
+                for(int j =i*i;j<=n;j+=i){
+                    arr[j]=false;
+                }
+            }
+        }
+        List<Integer> primes = new ArrayList<>();
         for(int i=m;i<=n;i++){
-            if(isPrime(i)){
+            if(arr[i]){
                 primes.add(i);
             }
         }
-        for(int i=0;i<primes.size();i++){
-            System.out.println(primes.get(i));
-        }
+        return primes;
     }
-    
-    private static boolean isPrime(int num){
-        if(num<2){
-            return false;
-        }
-        if(num==2){
-            return true;
-        }
-        if(num%2==0){
-            return false;
-        }
-        for(int i=3;i*i<=num;i+=2){
-            if(num%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    
 }
